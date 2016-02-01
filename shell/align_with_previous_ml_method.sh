dc1bde4a0be1af9593c84e955d248a17d634e0eb
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2016-02-01 16:15:35 +0900

    New: keyword feature

--- shell/align_with_previous_ml_method.sh
+++ shell/align_with_previous_ml_method.sh
@@ -2,18 +2,44 @@
 
 set -ue
 
+#類似度のみ
+function sim_only(){
+    CROSS_NUM=$1
+    SIM_ONLY_DIR=feature/previous_method/sim_only
+    cat list/patent_id_list.txt| python3 src/output_feature_by_previous_method.py > $SIM_ONLY_DIR/all.feature
+    cat $SIM_ONLY_DIR/all.feature | grep "^1 " > $SIM_ONLY_DIR/pos_sample.feature
+    cat $SIM_ONLY_DIR/all.feature | grep "^-1 " > $SIM_ONLY_DIR/neg_sample.feature
+
+
+    #正例の3倍だけ、負例をサンプリング
+    pos_sample_num=`cat $SIM_ONLY_DIR/pos_sample.feature | grep -c ""`
+    sort -R $SIM_ONLY_DIR/neg_sample.feature | head -n $[$pos_sample_num * 3] > $SIM_ONLY_DIR/neg_sample_extracted.feature
+    cat $SIM_ONLY_DIR/pos_sample.feature \
+        $SIM_ONLY_DIR/neg_sample_extracted.feature > $SIM_ONLY_DIR/sample.feature
+
+    #学習
+    svm-train -v $CROSS_NUM -h 0 $SIM_ONLY_DIR/sample.feature > $SIM_ONLY_DIR/result.txt
+}
+
+#類似度に加えて手掛かり語の素性を加えて評価
+function sim_and_keywords(){
+    CROSS_NUM=$1
+    SIM_AND_KEYWORDS_DIR=feature/previous_method/sim_and_keywords
+    cat list/patent_id_list.txt| python3 src/output_feature_by_previous_method.py --keywords > $SIM_AND_KEYWORDS_DIR/all.feature
+    cat $SIM_AND_KEYWORDS_DIR/all.feature | grep "^1 " > $SIM_AND_KEYWORDS_DIR/pos_sample.feature
+    cat $SIM_AND_KEYWORDS_DIR/all.feature | grep "^-1 " > $SIM_AND_KEYWORDS_DIR/neg_sample.feature
+
+
+    #正例の3倍だけ、負例をサンプリング
+    pos_sample_num=`cat $SIM_AND_KEYWORDS_DIR/pos_sample.feature | grep -c ""`
+    sort -R $SIM_AND_KEYWORDS_DIR/neg_sample.feature | head -n $[$pos_sample_num * 3] > $SIM_AND_KEYWORDS_DIR/neg_sample_extracted.feature
+    cat $SIM_AND_KEYWORDS_DIR/pos_sample.feature \
+        $SIM_AND_KEYWORDS_DIR/neg_sample_extracted.feature > $SIM_AND_KEYWORDS_DIR/sample.feature
+
+    #学習
+    svm-train -v $CROSS_NUM -h 0 $SIM_AND_KEYWORDS_DIR/sample.feature > $SIM_AND_KEYWORDS_DIR/result.txt
+}
+
 CROSS_NUM=4
-
-cat list/patent_id_list.txt| python3 src/output_feature_by_previous_method.py > feature/previous_method/all.feature
-cat feature/previous_method/all.feature | grep "^1 " > feature/previous_method/pos_sample.feature
-cat feature/previous_method/all.feature | grep "^-1 " > feature/previous_method/neg_sample.feature
-
-
-#正例の3倍だけ、負例をサンプリング
-pos_sample_num=`cat feature/previous_method/pos_sample.feature | grep -c ""`
-sort -R feature/previous_method/neg_sample.feature | head -n $[$pos_sample_num * 3] > feature/previous_method/neg_sample_extracted.feature
-cat feature/previous_method/pos_sample.feature \
-    feature/previous_method/neg_sample_extracted.feature > feature/previous_method/sample.feature
-
-#学習
-svm-train -v $CROSS_NUM -h 0 feature/previous_method/sample.feature > feature/previous_method/result.txt
+sim_only $CROSS_NUM
+sim_and_keywords $CROSS_NUM
--- src/output_feature_by_previous_method.py
+++ src/output_feature_by_previous_method.py
@@ -1,8 +1,23 @@
+"""Output Feature by Previous Method
+
+Usage: output_feature_by_previous_method.py [-h | --help]
+       output_feature_by_previous_method.py --keywords
+
+Options:
+    -h --help  show this help message and exit
+    --keywords  類似度だけでなく、手掛かり語の情報も素性に加える
+"""
+
+from docopt import docopt
 import sys
 import utils
+import collections
+import copy
 
 #patent_idの一覧を標準入力から読み込む
 def main():
+    args = docopt(__doc__)
+
     similarity_file_path = '/home/lr/tsakaki/work/aligning_patent_claims/list/similarity_list.txt'
     similarity_dic = utils.get_similarity_dic(similarity_file_path)
 
@@ -25,22 +40,48 @@ def main():
                 dp_match_sim = similarity_dic[key][1]
                 dp_mod_sim = similarity_dic[key][2]
                 
+                feature_lst = [] #素性値(float)を値とするリスト。このリストの0番目が素性番号1に対応するので、扱いに注意が必要。
+                feature_lst.append(rouge_sim)
+                feature_lst.append(dp_match_sim)
+                feature_lst.append(dp_mod_sim)
 
-                vec_str = "1:%f 2:%f 3:%f" % (rouge_sim, dp_match_sim, dp_mod_sim)
-                if claim_id in gold_answers:
-                    print("1 " + vec_str)
-                else:
-                    print("-1 " + vec_str)
+                if(args["--keywords"]):
+                    feature_lst = append_keyword_feature(feature_lst, detail_wakati_lst, claim_wakati_lst)
 
+                feature_str = get_feature_str(claim_id, gold_answers, feature_lst)
+                print(feature_str)
 
     return
 
+#素性リストに手がかり語に関する素性を加え、新しい素性リストを返す
+def append_keyword_feature(feature_lst, detail_wakati_lst, claim_wakati_lst):
+    ans = copy.deepcopy(feature_lst)
+    keywords = ["特徴", "上記", "目的", "形成",
+                "記載", "課題", "可能", "解決",
+                "発明", "構成", "有する", "係る",
+                "本発明", "防止", "配置", "実施",
+                "請求項", "提供", "効果", "検出"]
 
+    #TODO FIXME
+    #請求項と詳細説明に出現する手掛かり語の頻度数を素性に加える
+    counter_dic = collections.Counter(detail_wakati_lst + claim_wakati_lst)
+    for keyword_ind,keyword in enumerate(keywords):
+        cnt = counter_dic[keyword]
+        ans.append(cnt)
 
+    return ans
 
+def get_feature_str(claim_id, gold_answers, feature_lst):
+    ans_lst = []
+    if claim_id in gold_answers:
+        ans_lst.append("1")
+    else:
+        ans_lst.append("-1")
 
+    for feature_ind, feature_val in enumerate(feature_lst):
+        ans_lst.append("%d:%f" % (feature_ind + 1, feature_val)) #この+1がポイント。素性番号は0ではなく1から始まる
 
-
+    return " ".join(ans_lst)
 
 if __name__ == '__main__':
     main()
